af1d0ea059724cd9b059bfc0c46c85cda17a0794
Jon Hurst <jon.a@hursts.org.uk>
2026-06-22 14:55:22 +0100

    mvp

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
-"use strict";
+import { DateTime, Duration } from "./luxon.js";
+
 
 const ID = id => document.getElementById(id);
 
@@ -23,8 +24,8 @@ function _update(msg, state, draw) {
             state.dist_timestamp = (new Date()).getTime();
             break;
         case 3:
-            state.sta = parseInt(ID("sta").value);
-            state.eta = parseInt(ID("eta").value);
+            state.sta = ID("sta").value;
+            state.eta = ID("eta").value;
             state.tz_offset = parseInt(ID("tz").value);
             do_calculation(state);
         }
@@ -51,12 +52,28 @@ function fuzzy_bearing(bearing) {
 }
 
 function do_calculation(state) {
-    const nm_to_km = nm => (nm * 1.852).toFixed(2).toString();
+    const nm_to_km = nm => (nm * 1.852).toFixed(0).toString();
     state.out = {};
-    state.out.altitude = (state.fl * 0.03048).toFixed(2).toString();
+    state.out.altitude = (state.fl * 0.03048).toFixed(1).toString();
     state.out.track = fuzzy_bearing(state.track);
     state.out.speed = nm_to_km(state.gs);
-    state.out.s_per_km = (3600 / (state.gs * 1.852)).toFixed(2).toString();
+    state.out.s_per_km = (3600 / (state.gs * 1.852)).toFixed(0).toString();
+    // intention is to update these fields based on time stamp and gs
+    state.out.dist_to_run = nm_to_km(state.dist_left);
+    state.out.total_dist = nm_to_km(state.dist_total);
+    state.out.fraction_left = (state.dist_left / state.dist_total).toFixed(2).toString();
+    // intention is to update these fields based on time stamp and gs
+    state.out.wp_dist = nm_to_km(state.wp_distance);
+    state.out.wp_bearing = fuzzy_bearing(state.wp_bearing + 180);
+    state.out.wp_name = state.wp_name;
+    // date calculation uses luxon
+    let eta_z = DateTime.fromFormat(state.eta, "HHmm", { zone: "UTC" });
+    let sta_z = DateTime.fromFormat(state.sta, "HHmm", { zone: "UTC" });
+    let zone = state.tz_offset < 0 ? `UTC-${-state.tz_offset}` : `UTC+${state.tz_offset}`;
+    state.out.eta_l = eta_z.setZone(zone).toFormat("HH:mm");
+    state.out.eta_uk = eta_z.setZone("Europe/London").toFormat("HH:mm");
+    state.out.now_l = DateTime.now().setZone(zone).toFormat("HH:mm");
+    state.out.delay = Math.round((eta_z - sta_z) / 60000);
     console.log(state);
 }
 
@@ -88,6 +105,16 @@ function _draw(state) {
         ID("o-fp-fuzzy-trk").innerText = state.out.track;
         ID("o-fp-speed").innerText = state.out.speed;
         ID("o-fp-secs-per-km").innerText = state.out.s_per_km;
+        ID("o-dist-left").innerText = state.out.dist_to_run;
+        ID("o-dist-sector").innerText = state.out.total_dist;
+        ID("o-dist-left-fraction").innerText = state.out.fraction_left;
+        ID("o-wp-dist").innerText = state.out.wp_dist;
+        ID("o-wp-fuzzy-brg-from").innerText = state.out.wp_bearing;
+        ID("o-wp-name").innerText = state.out.wp_name;
+        ID("o-eta-uk").innerText = state.out.eta_uk;
+        ID("o-eta-l").innerText = state.out.eta_l;
+        ID("o-now-l").innerText = state.out.now_l;
+        ID("o-delay").innerText = state.out.delay;
     }
 }
 
