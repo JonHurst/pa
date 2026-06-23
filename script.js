53685d6fa49fbc4c6b3ea05ec2d3e837c789a571
Jon Hurst <jon.a@hursts.org.uk>
2026-06-23 08:05:53 +0100

    Changed input method

--- script.js
+++ script.js
@@ -3,45 +3,53 @@ import { DateTime, Duration } from "./luxon.js";
 
 const ID = id => document.getElementById(id);
 
+
+function extract_integer_fields(s, n) {
+    let fields = s.split("/").map(i => parseInt(i)).filter(i => !isNaN(i));
+    return fields.length == n ? fields : null;
+}
+
+
 function _update(msg, state, draw) {
+    console.log(msg);
+    let fields;
     switch(msg.type) {
-    case "next-stage":
-        switch(state.stage) {
-        case 0:
-            state.fl = parseInt(ID("fl").value);
-            state.track = parseInt(ID("track").value);
-            state.gs = parseInt(ID("gs").value);
-            break;
-        case 1:
-            state.wp_name = ID("waypoint").value;
-            state.wp_bearing = parseInt(ID("bearing").value);
-            state.wp_distance = parseInt(ID("distance").value);
-            state.wp_timestamp = (new Date()).getTime();
-            break;
-        case 2:
-            state.dist_total = parseInt(ID("dtot").value);
-            state.dist_left = parseInt(ID("dtg").value);
-            state.dist_timestamp = (new Date()).getTime();
-            break;
-        case 3:
-            state.sta = ID("sta").value;
-            state.eta = ID("eta").value;
-            state.tz_offset = parseInt(ID("tz").value);
-            do_calculation(state);
-        }
-        state.stage++;
-        break;
-    case "prev-stage":
-        state.stage--;
-        break;
-    case "recalc":
-        if(state.stage == 4) {
-            do_calculation(state);
-        } else {
+    case "flight-param-change":
+        fields = extract_integer_fields(msg.value, 3);
+        if(!fields) {
+            state.fl = state.track = state.gs = undefined;
             return;
         }
+        [state.fl, state.track, state.gs] = fields;
+        break;
+    case "waypoint-change":
+        fields = extract_integer_fields(msg.value, 2);
+        if(!fields) {
+            state.wp_bearing = state.wp_distance = undefined;
+            return;
+        }
+        [state.wp_bearing, state.wp_distance] = fields;
+        state.wp_timestamp = (new Date()).getTime();
+        break;
+    case "distances-change":
+        fields = extract_integer_fields(msg.value, 2);
+        if(!fields) {
+            state.dist_left = state.dist_total = undefined;
+            return;
+        }
+        [state.dist_left, state.dist_total] = fields;
+        state.dist_timestamp = (new Date()).getTime();
+        break;
+    case "times-change":
+        fields = msg.value.split("/");
+        if(fields.length != 3) {
+            state.eta = state.sta = state.tz_offset = undefined;
+            return;
+        }
+        [state.eta, state.sta, state.tz_offset] = fields;
         break;
     }
+    do_calculation(state);
     draw(state);
 }
 
@@ -88,70 +96,48 @@ function do_calculation(state) {
     state.out.wp_bearing = fuzzy_bearing(new_bearing + 180);
     state.out.wp_name = state.wp_name;
     // date calculation uses luxon
-    let eta_z = DateTime.fromFormat(state.eta, "HHmm", { zone: "UTC" });
-    let sta_z = DateTime.fromFormat(state.sta, "HHmm", { zone: "UTC" });
-    let zone = state.tz_offset < 0 ? `UTC-${-state.tz_offset}` : `UTC+${state.tz_offset}`;
-    state.out.eta_l = eta_z.setZone(zone).toFormat("HH:mm");
-    state.out.eta_uk = eta_z.setZone("Europe/London").toFormat("HH:mm");
-    state.out.now_l = DateTime.now().setZone(zone).toFormat("HH:mm");
-    state.out.delay = Math.round((eta_z - sta_z) / 60000);
-    console.log(state);
+    if(state.eta && state.sta && state.tz_offset) {
+        let eta_z = DateTime.fromFormat(state.eta, "HHmm", { zone: "UTC" });
+        let sta_z = DateTime.fromFormat(state.sta, "HHmm", { zone: "UTC" });
+        let zone = state.tz_offset < 0
+            ? `UTC-${-state.tz_offset}`
+            : `UTC+${state.tz_offset}`;
+        state.out.eta_l = eta_z.setZone(zone).toFormat("HH:mm");
+        state.out.eta_uk = eta_z.setZone("Europe/London").toFormat("HH:mm");
+        state.out.now_l = DateTime.now().setZone(zone).toFormat("HH:mm");
+        state.out.delay = Math.round((eta_z - sta_z) / 60000);
+    }
+    else {
+        state.out.eta_l = state.out.eta_uk = state.out.now_l = state.out.delay = "";
+    }
 }
 
+
 function _draw(state) {
-    let sections = document.getElementsByTagName("section");
-    for(let c = 0; c < sections.length; c++) {
-        if(c < state.stage) {
-            sections[c].classList.remove("hidden");
-            for(let el of sections[c].getElementsByTagName("input")) {
-                el.setAttribute("readonly", "");
-            }
-            for(let el of sections[c].getElementsByTagName("button")) {
-                el.setAttribute("disabled", "");
-            }
-        } else if(c == state.stage) {
-            sections[c].classList.remove("hidden");
-            let inputs = sections[c].getElementsByTagName("input");
-            for(let el of inputs) {
-                el.removeAttribute("readonly");
-            }
-            for(let el of sections[c].getElementsByTagName("button")) {
-                el.removeAttribute("disabled");
-            }
-            if(inputs.length) {
-                inputs[0].focus();
-            }
-        } else {
-            sections[c].classList.add("hidden");
-        }
-    }
-    if(state.stage == sections.length - 1) {
-        ID("o-fp-alt").innerText = state.out.altitude;
-        ID("o-fp-fuzzy-trk").innerText = state.out.track;
-        ID("o-fp-speed").innerText = state.out.speed;
-        ID("o-fp-secs-per-km").innerText = state.out.s_per_km;
-        ID("o-dist-left").innerText = state.out.dist_to_run;
-        ID("o-dist-sector").innerText = state.out.total_dist;
-        ID("o-dist-left-fraction").innerText = state.out.fraction_left;
-        ID("o-wp-dist").innerText = state.out.wp_dist;
-        ID("o-wp-fuzzy-brg-from").innerText = state.out.wp_bearing;
-        ID("o-wp-name").innerText = state.out.wp_name;
-        ID("o-eta-uk").innerText = state.out.eta_uk;
-        ID("o-eta-l").innerText = state.out.eta_l;
-        ID("o-now-l").innerText = state.out.now_l;
-        ID("o-delay").innerText = state.out.delay;
-    }
+    ID("o-fp-alt").innerText = state.out.altitude;
+    ID("o-fp-fuzzy-trk").innerText = state.out.track;
+    ID("o-fp-speed").innerText = state.out.speed;
+    ID("o-fp-secs-per-km").innerText = state.out.s_per_km;
+    ID("o-dist-left").innerText = state.out.dist_to_run;
+    ID("o-dist-sector").innerText = state.out.total_dist;
+    ID("o-dist-left-fraction").innerText = state.out.fraction_left;
+    ID("o-wp-dist").innerText = state.out.wp_dist;
+    ID("o-wp-fuzzy-brg-from").innerText = state.out.wp_bearing;
+    ID("o-eta-uk").innerText = state.out.eta_uk;
+    ID("o-eta-l").innerText = state.out.eta_l;
+    ID("o-now-l").innerText = state.out.now_l;
+    ID("o-delay").innerText = state.out.delay;
 }
 
 function do_wiring(update) {
-    for(let i of ["fp-next", "wp-next", "d-next", "done"]) {
-        ID(i).addEventListener("click", () =>
-            update({type: "next-stage"}));
-    }
-    for(let i of ["wp-back", "d-back", "t-back", "edit"]) {
-        ID(i).addEventListener("click", () =>
-            update({type: "prev-stage"}));
-    }
+    ID("flight").addEventListener("input", () =>
+        update({type: "flight-param-change", value: ID("flight").value}));
+    ID("waypoint").addEventListener("input", () =>
+        update({type: "waypoint-change", value: ID("waypoint").value}));
+    ID("distances").addEventListener("input", () =>
+        update({type: "distances-change", value: ID("distances").value}));
+    ID("times").addEventListener("input", () =>
+        update({type: "times-change", value: ID("times").value}));
 }
 
 function main() {
@@ -166,12 +152,17 @@ function main() {
         });
         window.setInterval(() => r.update(), 1000 * 60);
     });
-    let state = {stage: 0};
+    let state = {};
     let draw = () => _draw(state);
-    draw(state);
     let update = msg => _update(msg, state, draw);
     do_wiring(update);
-    window.setInterval(() => update({type: "recalc"}), 1000 * 10);
+    // browsers sometimes leave fields populated, so update them all to initialise
+    update({type: "flight-param-change", value: ID("flight").value});
+    update({type: "waypoint-change", value: ID("waypoint").value});
+    update({type: "distances-change", value: ID("distances").value});
+    update({type: "times-change", value: ID("times").value});
+
+    // window.setInterval(() => update({type: "recalc"}), 1000 * 10);
 }
 
 window.onload = main;
