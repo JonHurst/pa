f5b6507ffc7b3ce5b1e816c021ffc0f6681f7da5
Jon Hurst <jon.a@hursts.org.uk>
2026-07-18 18:37:23 +0100

    Use new standardised serviceworker code

--- sw.js
+++ sw.js
@@ -1,4 +1,7 @@
-const CACHE = 'pa_cache.1.29';
+const VERSION = "1.30";
+const UID = "pa_cache";
+const CACHE = `${UID}.${VERSION}`;
+const SAFE_PAGE = "index.html";
 const MANIFEST = [
     "index.html",
     "styles.css",
@@ -11,9 +14,9 @@ const MANIFEST = [
 ];
 
 
-self.addEventListener('install', evt => {
+self.addEventListener('install', event => {
     self.skipWaiting();
-    evt.waitUntil(
+    event.waitUntil(
         caches.open(CACHE)
             .then(cache => cache.addAll(MANIFEST))
             .catch(err => {
@@ -27,22 +30,33 @@ self.addEventListener("activate", (event) => {
     event.waitUntil(Promise.all([
         caches.keys()
             .then(key_list => key_list.filter(k =>
-                k.startsWith("pa_cache") && k != CACHE))
+                k.startsWith(UID) && k != CACHE))
             .then(del_list => Promise.all(
                 del_list.map(k => caches.delete(k)))),
         self.clients.matchAll()
             .then(clients => {
                 for(let client of clients) {
-                    console.log("Refreshing client");
-                    client.navigate("index.html");
+                    client.navigate(SAFE_PAGE);
                 }
             })
     ]));
 });
 
 
-self.addEventListener('fetch', evt => {
-    evt.respondWith(
+self.addEventListener('fetch', event => {
+    event.respondWith(
         caches.open(CACHE)
-            .then(cache => cache.match(evt.request.url)));
+            .then(cache => cache.match(event.request.url))
+            .then(response => {
+                if(response) {
+                    return response;
+                } else {
+                    console.log(`Failed to find ${event.request.url} in ${CACHE}`);
+                    return new Response(null, {
+                        status: 404,
+                        statusText: `${event.request.url} not in ${CACHE}`
+                    });
+                }
+            })
+    );
 });
